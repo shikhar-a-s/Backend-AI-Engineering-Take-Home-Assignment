38ae033f9d9e60004bc66c4910d58863e3be677a
Shikhar0805 <shikhar0805@gmail.com>
2026-08-14 14:28:57 +0530

    Fixed ImageKit SDK upload API

--- image-pipeline/src/services/imagekit.js
+++ image-pipeline/src/services/imagekit.js
@@ -1,26 +1,18 @@
 const ImageKit = require('@imagekit/nodejs');
 
-function getClient() {
-  return new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY || undefined,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || undefined,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || undefined,
-  });
-}
+const client = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
 
-async function uploadBuffer(buffer, fileName, folder) {
+async function uploadBuffer(buffer, fileName, folder = '/') {
   if (!process.env.IMAGEKIT_PRIVATE_KEY) {
     throw new Error('IMAGEKIT_PRIVATE_KEY is not configured');
   }
 
-  const client = getClient();
-  // Upload as base64 to be safe across environments
-  const base64 = buffer.toString('base64');
-  const result = await client.upload({
-    file: base64,
+  const result = await client.files.upload({
+    file: await ImageKit.toFile(buffer, fileName),
     fileName,
     folder,
-    isBase64: true,
   });
 
   return result;
